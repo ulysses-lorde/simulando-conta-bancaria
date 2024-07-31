9730bb95a6eabb17d92b4fb7a742c68f3ce252b1
Ulysses Oliveira <102372800+ulysses-lorde@users.noreply.github.com>
2024-07-31 14:55:19 -0300

    feat: add user input handling for bank account creation

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o número da conta:");
+        int numeroConta = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Digite o número da agência:");
+        String agencia = scanner.nextLine();
+        
+        System.out.println("Digite o seu nome:");
+        String nomeCliente = scanner.nextLine();
+        
+        System.out.println("Digite o valor do primeiro depósito:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("\nOlá " + nomeCliente + ", obrigado por criar uma conta em\nnosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo\n R$" + saldo + " já está disponível para saque");
+
+        scanner.close();
     }
 }
